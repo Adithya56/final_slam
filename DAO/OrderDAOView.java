c3af44693e105d6367577514371675f3c7f92ffe
ADITHYA THANDRA <85007575+Adithya56@users.noreply.github.com>
2023-06-25 14:59:04 +0530

    Add files via upload

---
+++ DAO/OrderDAOView.java
@@ -0,0 +1,19 @@
+package eStoreProduct.DAO;
+
+import java.util.List;
+
+
+import eStoreProduct.model.OrdersViewModel;
+
+
+public interface OrderDAOView {
+	public List<OrdersViewModel> getorderProds();
+	public  OrdersViewModel OrdProductById(Integer productId);
+	public void cancelorderbyId(Integer productId);
+	 public String getShipmentStatus(int orderId);
+		public List<OrdersViewModel> sortProductsByPrice(List<OrdersViewModel> orderLists, String sortOrder);
+
+public List<OrdersViewModel> filterProductsByPriceRange(List<OrdersViewModel> productList, double minPrice,
+			double maxPrice);
+	// public List<OrdersViewModel> getsortbypricel_low_to_high();
+}
